9a4f6ed5d01daa21440eb8f803e34a05812d9746
netzsooc <netzsooc@gmail.com>
2018-04-03 20:05:03 -0500

    Comentarios agregados y programas mejorados

--- etiqueta.py
+++ etiqueta.py
@@ -1,3 +1,25 @@
+#!/usr/bin/env python
+
+"""Este programa toma los discursos en formato txt y regresa un JSON
+etiquetado para cada uno de ellos
+
+Returns:
+    json -- archivo en formato json de los discursos. Es una lista de listas
+    donde cada una es a su vez una lista de objetos. Cada objeto es una
+    palabra etiquetada usando el servicio de freeling hospedado en:
+    http://www.corpus.unam.mx/servicio-freeling.
+    El objeto palabra contiene el siguiente formato:
+        'token': str -- la cadena como aparece en el texto
+        'lemma': str -- el lema del token (puede tener errores)
+        'tag': str -- etiqueta en formato eagles que contiene información
+               morfosintáctica de la palabra.
+        'prob': str -- cadena de un flotante  entre 0 y 1 que contiene la
+                certeza de la etiqueta asignada.
+"""
+
+__author__ = 'NetzSoOc'
+__date__ = '2018-04-03'
+
 import os
 import json
 import requests
@@ -11,7 +33,25 @@ def main():
                   encoding='utf8') as f:
             json.dump(etiquetado, f)
 
+
 def etiqueta_archivo(raiz, archivo, outf='tagged', formato='json'):
+    """Utiliza la API de http://www.corpus.unam.mx/servicio-freeling para
+    tokenizar y etiquetar un texto.
+    
+    Arguments:
+        raiz {str} -- directorio raiz donde se encuentra el archivo que se va
+                      a etiquetar.
+        archivo {str} -- archivo que se va a etiquetar
+    
+    Keyword Arguments:
+        outf {str} -- tipo de etiquetado que se desea, puede ser 'tagged',
+                      'parsed', 'dep'. (default: {'tagged'})
+        formato {str} -- formato de la respuesta, puede ser 'plain', 'json',
+                         'html'. (default: {'json'})
+    
+    Returns:
+        json -- objeto tipo json que contiene el documento etiquetado.
+    """
     archivo = {'file': open(raiz + archivo, mode='rb')}
     params = {'outf': outf, 'format': formato}
     url = 'http://www.corpus.unam.mx/servicio-freeling/analyze.php'
--- saca_estads.py
+++ saca_estads.py
@@ -1,1 +1,70 @@
+#!/usr/bin/env python
+
+"""Programa diseñado para sacar cuentas de las cosas de los candidatos
+"""
+
+__author__ = 'NetzSoOc'
+__date__ = '2018-04-03'
+
+
 import json
+import os
+from collections import defaultdict
+
+
+def main():
+    raiz = 'Datos/Etiquetados/'
+    for archivo in os.listdir(raiz):
+        with open(raiz + archivo, 'r', encoding='utf8') as f:
+            mi_json = json.load(f)
+        texto = Proceso(mi_json)
+        print(archivo)
+        num_oraciones = texto.num_oraciones
+        num_palabras = texto.num_palabras
+        mas_comun = texto.mas_comun
+        top_ten = texto.top_ten
+        prom_palabra = texto.prom_palabra
+
+        print("""
+        Cantidad de oraciones: {oraciones}
+        Cantidad de palabras: {palabras}
+        Promedio de oracion: {promedio}
+        Promedio de palabra: {prom_palabra}
+        Palabra más representativa: {mas_comun}
+        Top-10 palabras más representativas: {top_ten}
+        """.format(oraciones=num_oraciones, palabras=num_palabras,
+        mas_comun=mas_comun, top_ten=top_ten,
+        promedio=num_palabras/num_oraciones, prom_palabra=prom_palabra)
+        )
+
+class Proceso(object):
+    def __init__(self, etiquetado):
+        self.num_oraciones = len(etiquetado)
+        mis_palabras = self.mide_palabras(etiquetado)
+        self.num_palabras = len(mis_palabras)
+        self.prom_palabra = sum(mis_palabras)/self.num_palabras
+        self.mas_comun = self.palabras_comunes(etiquetado)[0]
+        self.top_ten = self.palabras_comunes(etiquetado)[:10]
+    
+    def mide_palabras(self, etiquetado):
+        longitud = []
+        for oracion in etiquetado:
+            for w in oracion:
+                longitud.append(len(w['token']))
+        return longitud
+    
+    def cuenta_palabras(self, etiquetado):
+        cuenta = 0
+        for oracion in etiquetado:
+            cuenta += len(oracion)
+        return cuenta
+    
+    def palabras_comunes(self, etiquetado):
+        for oracion in etiquetado:
+            oracion
+            pass
+        return [0]
+
+
+if __name__ == '__main__':
+    main()
