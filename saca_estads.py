2546fe900d63d2e2256342b3f1a78ca57a940eed
Octavio Sánchez <oct_sanc@unam.mx>
2018-04-04 01:27:36 -0500

    Agregado una función para contar palabras de los discursos

--- saca_estads.py
+++ saca_estads.py
@@ -9,7 +9,8 @@ __date__ = '2018-04-03'
 
 import json
 import os
-from collections import defaultdict
+from math import log
+from collections import defaultdict, Counter
 
 
 def main():
@@ -31,20 +32,23 @@ def main():
         Promedio de oracion: {promedio}
         Promedio de palabra: {prom_palabra}
         Palabra más representativa: {mas_comun}
-        Top-10 palabras más representativas: {top_ten}
         """.format(oraciones=num_oraciones, palabras=num_palabras,
-        mas_comun=mas_comun, top_ten=top_ten,
-        promedio=num_palabras/num_oraciones, prom_palabra=prom_palabra)
+        mas_comun=mas_comun, promedio=num_palabras/num_oraciones,
+        prom_palabra=prom_palabra)
         )
+        print('        Top-10 palabras representativas:')
+        for palabra in top_ten:
+            print('                {}'.format(palabra[0]))
 
 class Proceso(object):
     def __init__(self, etiquetado):
         self.num_oraciones = len(etiquetado)
         mis_palabras = self.mide_palabras(etiquetado)
+        comunes = self.palabras_comunes(etiquetado)
         self.num_palabras = len(mis_palabras)
         self.prom_palabra = sum(mis_palabras)/self.num_palabras
-        self.mas_comun = self.palabras_comunes(etiquetado)[0]
-        self.top_ten = self.palabras_comunes(etiquetado)[:10]
+        self.mas_comun = comunes.most_common(1)[0][0]
+        self.top_ten = comunes.most_common(10)
     
     def mide_palabras(self, etiquetado):
         longitud = []
@@ -60,10 +64,14 @@ class Proceso(object):
         return cuenta
     
     def palabras_comunes(self, etiquetado):
+        target_tags = ('A', 'R', 'PP', 'N', 'VM')
+        palabras = Counter()
         for oracion in etiquetado:
-            oracion
-            pass
-        return [0]
+            mis_palabras = [w['lemma'] for w in oracion if
+                            w['tag'].startswith(target_tags)]
+            palabras += Counter(mis_palabras)
+        return palabras
+        
 
 
 if __name__ == '__main__':
